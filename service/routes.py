00814af334b3319225c861d9607489bff0732d33
hobbs-chris <hobbs.chris.m@gmail.com>
2023-07-20 18:51:53 -0400

    added delete, update, list test cases and delete, update routes

--- service/routes.py
+++ service/routes.py
@@ -87,15 +87,45 @@ def read_account(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_accounts(account_id):
+    """
+    Update an existing accout
+    This endpoint will update an existing Account
+    """
+    app.logger.info("Request to update an Account with id: %s", account_id)
+    # use the Account.find() method to find the account
+    account = Account.find(account_id)
+    # abort() with a status.HTTP_404_NOT_FOUND if it cannot be found
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] not found.")
+    # call the deserialize() method on the account passing in request.get_json()
+    account.deserialize(request.get_json())
+    # call account.update() to update the account with the new data
+    account.update()
+    # return the serialize() version of the account with a return code of status.HTTP_200_OK
+    return account.serialize(), status.HTTP_200_OK
+
 
 
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
 
-# ... place you code here to DELETE an account ...
-
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_account(account_id):
+    """
+    Delete an Account
+    This endpoint will delete an Account based on id
+    """
+    app.logger.info("Request to delete an Account with id: %s", account_id)
+    # use the Account.find() method to retrieve the account by the account_id
+    account = Account.find(account_id)
+    # abort() with a status.HTTP_404_NOT_FOUND if it cannot be found
+    if account:
+        account.delete()
+    # return and empty body ("") with a return code of status.HTTP_204_NO_CONTENT
+    return "", status.HTTP_204_NO_CONTENT
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
